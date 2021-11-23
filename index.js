af4fe3870f467002732b78211bbf6fd445893315
Christopher Cosma <cosma.5@osu.edu>
2021-11-22 23:49:12 -0500

    Export Scraper class and constants

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const constants = require('./Constants');
 const Scraper = require('./Scraper');
 
-let s = new Scraper();
-s.getCurrentSeasonAndWeek(constants.ScraperConstants.Men, 2021, data => {
-    console.log(data);
-}); 
-
-// s.getFinalResults(constants.ScraperConstants.Men, 2020, data => {
-//     console.log(data);
-// });
+module.exports = {
+    Scraper: Scraper,
+    Constants: constants
+};
